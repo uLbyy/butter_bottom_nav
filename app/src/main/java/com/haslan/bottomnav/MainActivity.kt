deb259d1c07fad15ba58db44ba99a2ac8e34a251
huseyinaslan <huseyin.aslan@outlook.com>
2020-06-26 11:54:07 +0300

    Sample icons changed.

--- app/src/main/java/com/haslan/bottomnav/MainActivity.kt
+++ app/src/main/java/com/haslan/bottomnav/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.haslan.butterbottomnav.ButterBottomNavListener
+import com.haslan.butterbottomnav.ButterBottomNavUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ButterBottomNavListener {
@@ -12,10 +13,11 @@ class MainActivity : AppCompatActivity(), ButterBottomNavListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         butter_bottom_nav.initListener(this)
+        butter_bottom_nav.tabPositionChanged(ButterBottomNavUtils.TAB_ONE)
     }
 
     override fun tabClicked(tab: Int) {
-        Toast.makeText(this, tab.toString(), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "$tab tab licked", Toast.LENGTH_SHORT).show()
         butter_bottom_nav.tabPositionChanged(tab)
     }
 
